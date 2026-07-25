0b49ce2ac01fd82fe2687e245b1e050448907205
ryzq99 <semamgat234@gmail.com>
2026-07-25 18:41:23 +0700

    Update database seeder with new user entries and add phpMyAdmin service to Docker configuration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,30 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::truncate(); // Menghapus seluruh data user sebelumnya (opsional)
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@sig.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin123'),
+        ]);
+
+        User::create([
+            'name' => 'Rizky Anugrah',
+            'email' => 'rizky@sig.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('rizky123'),
         ]);
     }
 }
